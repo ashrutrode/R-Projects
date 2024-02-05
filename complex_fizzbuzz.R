bfc8d26e3fb9f9ee1fce67e25ef4b369cb22ffae
Ashrut Rode <143514581+ashrutrode@users.noreply.github.com>
2024-02-05 18:56:07 -0500

    Create complex_fizzbuzz.R

---
+++ complex_fizzbuzz.R
@@ -0,0 +1,10 @@
+final_positive_int <- 100
+fizzbuzz <- function(num) {
+	for (i in 1:num) {
+		if (i%%3 == 0 & i%%5 == 0) {print("FizzBuzz")}
+		else if (i%%3 == 0) {print("Fizz")}
+		else if (i%%5 == 0) {print("Buzz")}
+  	else {print(i)}
+	}
+}
+fizzbuzz(final_positive_int)
